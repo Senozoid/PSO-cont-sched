cc23c5dc1823167569cb9b0bc7e52420a4d1b083
Senozoid <93595764+Senozoid@users.noreply.github.com>
2023-03-29 10:56:23 +0530

    class Optimizer: proper comments

--- src/Optimizer.java
+++ src/Optimizer.java
@@ -12,6 +12,7 @@ public class Optimizer {
     public static void main(String[] args){
         int i=0;
 
+        //try to initiate logger, exit if failed
         boolean status=false;
         try{
             logger=new PrintStream(log);
@@ -24,10 +25,14 @@ public class Optimizer {
         if(!status) System.exit(-1);
 
         System.out.println("\nThis program attempts to use PSO for container scheduling\n");
+
+        //start taking inputs and initialize stuff
         int epochs = takeInt("Enter the number of epochs: ");
         int[] space = takeSpace();
         FitnessDetails fitness = new FitnessDetails(space[1],space[2]);
         Swarm hive = new Swarm(space,fitness);
+
+        //run optimization
         System.out.print("\n");
         try {
             i=search(hive,epochs);
@@ -38,6 +43,7 @@ public class Optimizer {
         declare(hive);
     }
 
+    //search the space for the given number of epochs, with the given swarm
     private static int search(Swarm hive, int epochs) throws InterruptedException {
         int i;
         float[] parameters = takeParameters();
@@ -55,12 +61,14 @@ public class Optimizer {
         return i;
     }
 
+    //announce the global best position so far and its fitness value
     private static void declare(Swarm hive){
         System.out.print("          \n");
         System.out.println("\tGlobal best position = "+hive.getBestPos().toString());
         System.out.println("\tGlobal best evaluation = "+hive.getBestEval());
     }
 
+    //take swarm size, number of containers (dimension) and number of hosts (upper bound, exclusive) from user input
     private static int[] takeSpace(){
         int[] space = new int[3];
         space[0]=takeInt("Enter the number of particles: ");
@@ -69,6 +77,7 @@ public class Optimizer {
         return space;
     }
 
+    //take inertia (<1), cognitive component and social component from user input
     private static float[] takeParameters(){
         float[] parameters = new float[4];
         parameters[0]=takeFloat("Enter inertia: ",1);
@@ -78,6 +87,7 @@ public class Optimizer {
         return parameters;
     }
 
+    //take int input from user (>0)
     private static int takeInt(String msg) {
         int num;
         while (true) {
@@ -96,6 +106,7 @@ public class Optimizer {
         return num;
     }
 
+    //take float input from user (0 < input < given limit)
     private static float takeFloat(String msg, float limit) {
         float num;
         while (true) {
@@ -114,6 +125,7 @@ public class Optimizer {
         return num;
     }
 
+    //log the position and velocity of all particles in the given swarm
     private static void logParticles(Swarm hive, int i){
         System.setOut(logger);
         System.out.println("\nEPOCH "+i+" :\n");
@@ -124,6 +136,7 @@ public class Optimizer {
         System.setOut(console);
     }
 
+    //attempt to clear the console output screen
     public static void cls(){
         try{
             if(System.getProperty("os.name").contains("Windows"))
